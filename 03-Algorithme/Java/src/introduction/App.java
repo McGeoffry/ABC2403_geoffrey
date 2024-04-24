15ffe44515959fa48b6c9e8a4a3dfcb26639dec0
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-04-24 16:01:43 +0200

    Exo HelloWorld

---
+++ 03-Algorithme/Java/src/introduction/App.java
@@ -0,0 +1,46 @@
+package introduction;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		// Affiche "Salut mon Ami!" dans la console
+		
+		System.out.println("Salut mon Ami !");  
+		
+		
+		// VARIABLES
+		
+		String prenom; // Déclaration d'une variable "prenom" de type "chaine de caractères"
+		
+		int age; // Déclaration d'une variable "age" de type 'Nombre entier" (valeur scolaire) (plage de valeurs d'un INT -2 147 000 000 à +2 147 000 000)
+		
+		long soldeBancaire; // Déclaration d'une variable "soldeBancaire" de type 'Nombre entier 64 bits" (valeur scolaire)
+		
+		float tailleEnMettre; // Déclaration d'une variable "tailleEnMettre" de type 'Nombre réel 32 bites" (valeur scolaire)
+		
+		double tailleEnMettrePourLesPlusGrands; // Déclaration d'une variable "tailleEnMettrePourLesPlusGrands" de type 'Nombre réel 64 bites" (valeur scolaire)
+		
+		Boolean estMajaur; // Déclaration d'une variable "estMajeur" de type "Boolean" (valeur scolaire)
+		
+		
+		prenom = "McGeoffry"; // affectation d'une valeur à la variable "prenom"
+		
+		System.out.println(prenom);
+		
+		
+		prenom = "Toto"; // modification de la valeur de la variable "prenom"
+		
+		System.out.println("Bonjour " + prenom);
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ 03-Algorithme/Java/src/introduction/Program.java
@@ -0,0 +1,12 @@
+package introduction;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World !");
+		
+		
+	}
+
+}
