5a7253754434914403138ac03c2f7e9aca351862
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-04-25 12:53:38 +0200

    Exo Aire et Volume OK

--- 03-Algorithme/Exo4/src/calculAireEtSphere/App.java
+++ 03-Algorithme/Exo4/src/calculAireEtSphere/App.java
@@ -1,10 +1,41 @@
 package calculAireEtSphere;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		double nombre;
+		double aire;
+		double volume;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir votre rayon");
 
+		nombre = sc.nextDouble();
+		
+		
+		aire = 4.0 * Math.PI * Math.pow(nombre, 2);
+		
+		volume = 4.0 * Math.PI * Math.pow(nombre, 3) / 3;
+		
+		System.out.println("");
+		
+		System.out.println("L'aire de votre spère est d'un rayon est de " + aire);
+		
+		System.out.println("");
+		
+		System.out.println("Le volume de votre sphère est de " + volume);
+				
+				
+		sc.close();
+		
+		
 	}
 
 }
