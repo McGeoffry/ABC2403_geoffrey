b121a4d932e113c8481960d4f8759858adac36d0
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-05-17 09:52:45 +0200

    exoLesNombresSuivants

--- 03-Algorithme/exoBicyclette/src/exoBycyclette/App.java
+++ 03-Algorithme/exoBicyclette/src/exoBycyclette/App.java
@@ -6,48 +6,43 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Boolean beauTemps = true;
-		Boolean veloOk = true;
-		Boolean reparationsImmediates = true;
-		Boolean livrePossede = false;
-		Boolean livreDispoBibliotheque = true;
+		Boolean beauTemps;
+		Boolean veloOk;
+		Boolean reparationsImmediates;
+		Boolean livrePossede;
+		Boolean livreDispoBibliotheque;
 		
-		if(beauTemps == true) { 
+		
+		if(beauTemps = true) {
 			System.out.println("Je vais me balader");
-			
-           if(veloOk == true) {
-        	   System.out.println("J'irai me balader à bicyclette");
-           }
-           else {
-        	   System.out.println("Je vais chez le garagiste");
-           }
-           
-           		if(reparationsImmediates == true) {
-           			System.out.println("J'irai me balader à bicyclette");
-           		}
-           		else {
-           			System.out.println("J'irai cueillir des joncs");
-           		} 
-		}
-		else {
-			System.out.println("Je vais lire un livre");
-		}
-		
-		livrePossede = false;
-		if(livrePossede = false) {
-			System.out.println("Je vais à la bibilothèque");
-			livreDispoBibliotheque = true;
-			if(livreDispoBibliotheque = true) {
-				System.out.println("J'empreinte GoT");
+			if(veloOk = true) {
+				System.out.println("j'irai me ballader à bicyclette");
 			}
 			else {
-				System.out.println("J'empreinte un roman policier");
+				System.out.println("Je vais chez le garagiste");
 			}
+			if(reparationsImmediates = true) {
+				System.out.println("j'irai me ballader à bicyclette");
+			}
+			else {
+				System.out.println("J'irai cueillir des joncs");
+			}
+		}
+		
+		if(livrePossede = false) {
+			System.out.println("Je vais à la bibliothèque");
+			if(livreDispoBibliotheque = true) {
+				System.out.println("J'emprunte GoT");
+			}
+			else {
+				System.out.println("J'emprunte un roman policier");
+			}
+			System.out.println("je rentre chez moi");
+		}
+		else {
 			System.out.println("Je lis GoT");
 		}
-		System.out.println("Lescture de mon livre dansle fauteil");
-
-	
+		System.out.println("Lecture de mon livre dans le fauteuil");
 	}
 
 }
---
+++ 03-Algorithme/lesNombresSuivants/src/lesNombresSuivants/Boucles.java
@@ -0,0 +1,30 @@
+package lesNombresSuivants;
+
+import java.util.Scanner;
+
+public class Boucles {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
