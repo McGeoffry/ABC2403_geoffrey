6185a0883fa880f4dfc31d364438ceb94bdf56bd
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-05-21 12:05:28 +0200

    exoEtLePlusGrandEstFait

---
+++ 03-Algorithme/etLePlusGrandEst/src/README.md
@@ -0,0 +1,25 @@
+#Exercice 3.6 : Et le plus grand est…
+Érire un algorithme qui demande successivement 20 nombres à l’utilisateur, et qui lui inqique ensuite quel est le plus grand parmi ces 20 nombres.
+
+Exemple de fonctionnement du programme :
+
+
+
+
+```
+VARIABLE
+
+saisie est un NOMBRE ENTIER
+leNombreMax est un NOMBRE ENTIER
+nombre est un NOMBRE ENTIER
+
+
+ECRIRE "saisissez 20 nombres "
+POUR saisie = 1 ; saisie <= 20 
+ECRIRE "nombre "+ saisie + ": "
+SI nombre > leNombreMax ALORS
+LeNombreMax = nombre
+
+ECRIRE "Le plus grand nombre saisie est: " + leNombreMax
+
+FIN
---
+++ 03-Algorithme/etLePlusGrandEst/src/etLePlusGrandEst/Boucles.java
@@ -0,0 +1,33 @@
+package etLePlusGrandEst;
+import java.util.Scanner;
+
+public class Boucles {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int leNombreMax = Integer.MIN_VALUE; // Initialiser le maximum avec la valeur minimale possible pour un entier
+		
+		System.out.println("Saisissez 20 nombres ");
+		
+		// Demander 20 nombres à l'utilisateur
+		for (int saisie = 1; saisie <= 20; saisie++) {
+			System.out.println("Nombre " + saisie + ": ");
+			int nombre = sc.nextInt();
+			
+			// Mettre à jour le maximum si le nombre saisi est supérieur
+			if (nombre > leNombreMax) {
+				leNombreMax = nombre;
+			}
+		}
+		
+		// Afficher le plus grand nombre
+		System.out.println("Le plus grands nombre saisie est: " + leNombreMax );
+		
+		sc.close();
+
+
+	}
+
+}
