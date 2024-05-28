47298e97ade54e59c076162125cc00e973a24c3f
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-05-28 15:56:08 +0200

    exoFloatOk

--- 03-Algorithme/demoFonctions2/src/demoFonctions2/App.java
+++ 03-Algorithme/demoFonctions2/src/demoFonctions2/App.java
@@ -3,8 +3,18 @@ package demoFonctions2;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a;
+		
+		a = 20;
+		
+		App.changerValeur(a);
+		
+		System.out.println(a);
 	}
 
+	public static void changerValeur(int a)
+	{
+		a = 30;
+	}
 }
--- 03-Algorithme/exoFonctions/src/exoFonctions/App.java
+++ 03-Algorithme/exoFonctions/src/exoFonctions/App.java
@@ -16,12 +16,30 @@ public class App {
 	App.calculerPerimetreTriangle(a, b, c);
 		
 		// Le programme doit afficher : 90.
+	
+	int[] valeurs = new int[] {1, 3, 5, 7, 3, 4, 2, 4, 22};
+	App.calculerMoyenne(valeurs);
 	}
 	public static void calculerPerimetreTriangle(int a1, int b1, int c1)
 	{
 		int resultat = a1 + b1 + c1;
 
-		System.out.println("Le périmètre du triangle est de " + resultat);
+		System.out.println("Le périmètre du triangle est de : " + resultat);
 	
+	}
+	// calcule et affiche la moyenne des valeurs d'un tableau
+			// @param tableau le tableau de valeurs
+	
+	public static void calculerMoyenne(int[] tableau) {
+		int somme;
+		float moyenne;
+		
+		for(int i = 0; i < tableau.length; i++) {
+			//somme = somme + tableau[i];
+			somme += tableau[i];
+		}
+		moyenne = (float)somme / (float)tableau.length;
+		
+		System.out.println(moyenne);
 	}
 }
