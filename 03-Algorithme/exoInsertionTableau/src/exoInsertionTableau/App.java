59a5556e63e012f3b2de2708e8009169856e705d
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-05-30 13:06:13 +0200

    exoTableauAFinir

---
+++ 03-Algorithme/exoInsertionTableau/src/exoInsertionTableau/App.java
@@ -0,0 +1,22 @@
+package exoInsertionTableau;
+
+import java.util.*;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] tableau = new int[] {1, 4, 5, 10, 20, 22, 25, 30, 32};
+		int nombre;
+		int positionNb = Arrays.binarySearch(tableau, nombre);
+		System.out.println("Veuillez saisir la valeur à ajouter: " + nombre);
+		int positionNb = Arrays.binarySearch(tableau + 1);
+		
+		public static int[] ()
+		
+		
+		
+		sc.close();
+	}
+
+}
