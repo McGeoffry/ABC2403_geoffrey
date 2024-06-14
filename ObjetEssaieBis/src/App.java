c293e7218223f7359d1629721fce91f54dfbf86d
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-06-14 09:58:47 +0200

    exoBouteilleAFinir

--- ObjetEssaieBis/src/App.java
+++ ObjetEssaieBis/src/App.java
@@ -3,11 +3,26 @@ public class App {
 
 	public static void main(String[] args) {
 	
+		System.out.println("coucou");
+		double resu = App.additionner(2, 3);
+		
+		
+		string prenom = "Geoffrey";
+		string nom = "Crevel";
+		String resu= App.prenomNom(prenom, nom);
+	}
+	
+	public static double additionner(double val1, double val2) {
+		double resultat = 0;
+		resultat = val1 + val2;
+		return resultat;
+	}
+	
+	public static String prenomNom(String prenom, String Nom) {
+
+		return prenom + Nom;
 		
 		
 	}
 	
-	public static double additionner
-	
-
 }
