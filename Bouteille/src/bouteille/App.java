c234ad8d5ade353910442706193e0bb85d0a6ca1
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-06-17 16:42:27 +0200

    laSuiteDemain

---
+++ Bouteille/src/bouteille/App.java
@@ -0,0 +1,13 @@
+package bouteille;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public class Bouteille {
+		
+	}
+	
+}
--- ObjetEssaieBis/src/App.java
+++ ObjetEssaieBis/src/App.java
@@ -3,16 +3,16 @@ public class App {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Bonjour");
+		/*System.out.println("Bonjour");
 		double resu = App.additionner(2, 3);
 	
 		String resultat = App.prenomNom("Geoffrey", "Crevel");
 		String prenom = "Geoffrey";
 		String nom = "Crevel";
-		System.out.println(resultat);
+		System.out.println(resultat);*/
 		
-		String resultat1 = App.disBonjour("Bonjour");
-		System.out.println("Bonjour" + resultat1);
+		String resultat1 = App.disBonjour("Geoffrey");
+
 	}
 	
 	public static double additionner(double val1, double val2) {
@@ -25,7 +25,7 @@ public class App {
 		
 	
 		String retour;
-		retour = prenom + nom;
+		retour = prenom + " " + nom;
 		return retour;
 		
 		
@@ -34,7 +34,8 @@ public class App {
 	public static String disBonjour(String prenom1) {
 		
 		String retour;
-		retour = prenom1;
+		retour = "Bonjour " +prenom1;
+		System.out.println(retour);
 		return retour;
 	}
 	
