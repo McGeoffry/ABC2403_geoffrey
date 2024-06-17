f30de76e42dd4b7062ddcb3d3841a6f566fc1261
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-06-17 09:56:21 +0200

    disBonjourexo

--- ObjetEssaieBis/src/App.java
+++ ObjetEssaieBis/src/App.java
@@ -3,13 +3,16 @@ public class App {
 
 	public static void main(String[] args) {
 	
-		System.out.println("coucou");
+		System.out.println("Bonjour");
 		double resu = App.additionner(2, 3);
+	
+		String resultat = App.prenomNom("Geoffrey", "Crevel");
+		String prenom = "Geoffrey";
+		String nom = "Crevel";
+		System.out.println(resultat);
 		
-		
-		string prenom = "Geoffrey";
-		string nom = "Crevel";
-		String resu= App.prenomNom(prenom, nom);
+		String resultat1 = App.disBonjour("Bonjour");
+		System.out.println("Bonjour" + resultat1);
 	}
 	
 	public static double additionner(double val1, double val2) {
@@ -18,11 +21,21 @@ public class App {
 		return resultat;
 	}
 	
-	public static String prenomNom(String prenom, String Nom) {
-
-		return prenom + Nom;
+	public static String prenomNom(String prenom, String nom) {
+		
+	
+		String retour;
+		retour = prenom + nom;
+		return retour;
 		
 		
+	}
+	
+	public static String disBonjour(String prenom1) {
+		
+		String retour;
+		retour = prenom1;
+		return retour;
 	}
 	
 }
