4b284e3a0eff72a907e770852c03a7798a099ebc
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-06-24 16:49:26 +0200

    BouteilleAFinir

--- Bouteille/src/bouteille/App.java
+++ Bouteille/src/bouteille/App.java
@@ -4,16 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 		
-	}
-
-	public class Bouteille {
-		private String nom;
-		private Date dateCreation;
-		
-	public Livre chercherLivre(ISBN isbn) {
+		Bouteille bouteille = new Bouteille();
+	
 			
 		}
 		
 	}
-	
-}
