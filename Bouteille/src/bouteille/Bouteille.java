1bc5bd7a75a4cba06825bfb5795a8f78646f7b80
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-06-24 16:47:34 +0200

    MalAuCraneADemain

--- Bouteille/src/bouteille/Bouteille.java
+++ Bouteille/src/bouteille/Bouteille.java
@@ -2,9 +2,42 @@ package bouteille;
 
 public class Bouteille {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private float contenanceEnL;
+	private float contenuEnL;
+	private boolean ouvert;
+	private String typeDuContenu;
+	
+	public Bouteille() {
+		contenanceEnL = 1.2f;
+		contenuEnL = 0.8f;
+		ouvert = false;
+		typeDuContenu = "coca";
+	}
+	
+	public Bouteille(float _contenanceEnL, float _contenuEnL, boolean _ouvert, String _typeDuContenu) {
+		this.contenanceEnL = _contenanceEnL;
+		this.contenuEnL = _contenuEnL;
+		this.ouvert = _ouvert;
+		this.typeDuContenu = _typeDuContenu;
+		
+	}
+	
+	public float donneContenanceEnCL() {
+		float result;
+		result = this.contenanceEnL / 100;
+		return result;
+	}
+	
+	public void donneContenuEnCL() {
+		
+		
 	}
 
+	public boolean estPleine() {
+	
+	}
+	
+	public boolean fermer() {
+		boolean 
+	}
 }
