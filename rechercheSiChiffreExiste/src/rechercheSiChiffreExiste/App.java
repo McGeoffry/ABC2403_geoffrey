ac113f29b61db75545f8e5843962fde4d116dc45
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-06-25 15:40:10 +0200

    continuerExoSophie

---
+++ rechercheSiChiffreExiste/src/rechercheSiChiffreExiste/App.java
@@ -0,0 +1,31 @@
+package rechercheSiChiffreExiste;
+
+public class App {
+
+	public static void main(String[] args) {
+		int nombre = 8;
+		
+		int resultat;
+		
+		int[] tab = new int[]{ 2, 4, 8 ,10 };
+		boolean resultat = App.rechercheSiChiffreExiste(tab, nombre);
+	}
+
+}
+
+
+
+	public static boolean rechercheSiChiffreExiste(int[] tab, ChiffreRecherche) {
+		
+		boolean trouve = false;
+			while(trouve == false) {
+				
+		
+		}
+		return trouve;
+	}
+	
+
+
+
+	
