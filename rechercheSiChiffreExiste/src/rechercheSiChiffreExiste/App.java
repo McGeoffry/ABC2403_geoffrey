ea4ce4cda780a10f6c1e69a3c9c5bac0aa03412e
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-06-26 15:48:23 +0200

    rdvMdmBilz

--- rechercheSiChiffreExiste/src/rechercheSiChiffreExiste/App.java
+++ rechercheSiChiffreExiste/src/rechercheSiChiffreExiste/App.java
@@ -3,29 +3,31 @@ package rechercheSiChiffreExiste;
 public class App {
 
 	public static void main(String[] args) {
+
 		int nombre = 8;
-		
-		int resultat;
-		
-		int[] tab = new int[]{ 2, 4, 8 ,10 };
+		int[] tab = new int[] { 2, 4, 8, 10 };
 		boolean resultat = App.rechercheSiChiffreExiste(tab, nombre);
+		if (resultat == false) {
+			
+			System.out.println("trouve");
+
+		} 
+		
+		
 	}
 
-}
-
-
-
-	public static boolean rechercheSiChiffreExiste(int[] tab, ChiffreRecherche) {
+	public static boolean rechercheSiChiffreExiste(int[] tab, int chiffreRecherche) {
 		
 		boolean trouve = false;
-			while(trouve == false) {
-				
+		int i = 0;
+		while(i <= chiffreRecherche) {
+			  
+		i++;
+		i++;
 		
 		}
 		return trouve;
+
 	}
-	
 
-
-
-	
+}
