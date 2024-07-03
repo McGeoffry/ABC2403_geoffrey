a66a637c34c4a4f0e1958f26a18e369440fcf951
McGeoffryX <166002244+McGeoffry@users.noreply.github.com>
2024-07-03 16:46:44 +0200

    palindromeOK

--- Palindrone/src/palindrome/App.java
+++ Palindrone/src/palindrome/App.java
@@ -3,31 +3,47 @@ package palindrome;
 public class App {
 
 	public static void main(String[] args) {
-		
-		String[] mot = new String[] { "L", "A", "V", "A", "L" };
 
-		boolean resultat = estUnPalindrome("LAVAL");
-		if(resultat == true){
-			for(int i = 0; i < mot.length ; i++) {
-			System.out.println(resultat + " est un palindrome");
+		String mot = "LAVAL";
+
+		boolean resultat = estUnPalindrome(mot);
+
+		if (resultat == true) {
+			for (int i = 0; i < mot.length(); i++) {
+				System.out.print(mot.charAt(i));
 			}
-			for(int j = 0; j < mot.length - 1; j++);
-	  }
-				
-	   {
-			boolean resultat2 = estUnPalindrome("COUCOU");
-			
-			
-		System.out.println(resultat2 + " n'est pas un palindrome");
-			
+			System.out.println(" est un palindrome");
+
 		}
+		else {
+			System.out.println(" n'est pas un palindrome");
+		}
+
+		String mot2="COUCOU";
+		boolean resultat2 = estUnPalindrome(mot2);
+		
+		for (int i = 0; i < mot2.length(); i++) {
+			System.out.print(mot2.charAt(i));
+		}
+
+		if (resultat2 == true) {
+			System.out.println(" est un palindrome");
+
+		}
+		else {
+			System.out.println(" n'est pas  un palindrome");	
+		}
+		
+			
+
+	
 	}
 
 	public static boolean estUnPalindrome(String mot) {
 		boolean ok = true;
 		int i = 0;
 		int j = mot.length() - 1 - i;
-		while (!ok == false && i < mot.length() / 2) {
+		while ( i < mot.length() / 2 && !ok ) {
 			if (mot.charAt(i) != mot.charAt(j)) {
 				ok = false;
 			}
